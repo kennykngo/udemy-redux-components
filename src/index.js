f511f4799a021ac5c04023c9f1b9f9d3522fadef
Kenny Ngo <kenny.ngo.design@gmail.com>
2020-12-11 21:47:44 -0800

    passed CommentDetails as ApprovalCard's children

--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
+import Faker from "faker";
 import React from "react";
 import ReactDOM from "react-dom";
 
+import ApprovalCard from "./ApprovalCard";
 import CommentDetail from "./CommentDetail";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Jane" />
-      <CommentDetail author="Sam" />
-      <CommentDetail author="Jane" />
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Today at 4:45pm"
+          content="Nice blog post"
+          avatar={Faker.image.image()}
+        />
+      </ApprovalCard>
+
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 4:00pm"
+        content="Nice writing"
+        avatar={Faker.image.image()}
+      />
+      <CommentDetail
+        author="Jane"
+        timeAgo="Today at 3:45pm"
+        content="Nice blog post"
+        avatar={Faker.image.image()}
+      />
     </div>
   );
 };
